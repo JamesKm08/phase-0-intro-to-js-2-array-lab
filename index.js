b82668d3577697a19c7a79aa6807ac2886c61af2
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-07-15 13:02:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name = 'Ralph'){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name = "Bob"){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    let newCat = [...cats, name];
+    return newCat
+}
+
+function prependCat(name){
+    let newCat = [name, ...cats];
+    return newCat
+}
+
+function removeLastCat(){
+    let newCat = cats.slice(0, cats.length - 1);
+    return newCat
+}
+
+function removeFirstCat(){
+    let newCat = cats.slice(1);
+    return newCat
+}
